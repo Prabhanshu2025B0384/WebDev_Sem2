f7646531af0a862b5f7447719aa09fc6e4603212
Prabhanshu2025B0384 <saxena.prabhanshu1001@gmail.com>
2026-04-09 16:05:31 +0530

    Practical file added

--- Concept Practice/sup/random.js
+++ Concept Practice/sup/random.js
@@ -1,11 +1,23 @@
-const add=(a,b)=>a+b;
-console.log(add(5,6));
+// const add=(a,b)=>a+b;
+// console.log(add(5,6));
 
 
-let arr=[1,2,3,4,5,6];
-let arrsqua=arr.map(value =>(value*value));
-console.log(arrsqua);
-let arrfilt=arrsqua.filter(value =>(value>10));
-console.log(arrfilt);
+// let arr=[1,2,3,4,5,6];
+// let arrsqua=arr.map(value =>(value*value));
+// console.log(arrsqua);
+// let arrfilt=arrsqua.filter(value =>(value>10));
+// console.log(arrfilt);
+// let sum = nums.reduce((acc, curr) => {return acc + curr;}, 0);
 
-let sum = nums.reduce((acc, curr) => {return acc + curr;}, 0);
+
+// let student={
+//     name:"Deepti",
+//     age:25
+// };
+// let jsonData=JSON.stringify(student);
+
+// console.log(jsonData);
+
+
+
+let jsonString='{"Name":"Deepti","age":25}';
